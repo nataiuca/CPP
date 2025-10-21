55da93988e711ed8f7fb4c666ce59a1322b46f55
nataiuca <movil.nataiuca@gmail.com>
2025-10-21 13:18:39 +0200

    Validate input fields in setContact method
    Updated setContact method to ensure fields are not empty.

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -3,20 +3,67 @@
 Contact::Contact() {}
 
 void Contact::setContact() {
+    std::string input;
+
     std::cout << "First name: ";
-    std::getline(std::cin, firstName);
+    std::getline(std::cin, input);
+    while (input.empty()) {
+        if (std::cin.eof()) {
+            std::cout << "\nExiting PhoneBook..." << std::endl;
+            exit(0);
+        }
+        std::cout << "❌ Field cannot be empty. Enter first name: ";
+        std::getline(std::cin, input);
+    }
+    firstName = input;
 
     std::cout << "Last name: ";
-    std::getline(std::cin, lastName);
+    std::getline(std::cin, input);
+    while (input.empty()) {
+        if (std::cin.eof()) {
+            std::cout << "\nExiting PhoneBook..." << std::endl;
+            exit(0);
+        }
+        std::cout << "❌ Field cannot be empty. Enter last name: ";
+        std::getline(std::cin, input);
+    }
+    lastName = input;
 
     std::cout << "Nickname: ";
-    std::getline(std::cin, nickname);
+    std::getline(std::cin, input);
+    while (input.empty()) {
+        if (std::cin.eof()) {
+            std::cout << "\nExiting PhoneBook..." << std::endl;
+            exit(0);
+        }
+        std::cout << "❌ Field cannot be empty. Enter nickname: ";
+        std::getline(std::cin, input);
+    }
+    nickname = input;
 
     std::cout << "Phone number: ";
-    std::getline(std::cin, phoneNumber);
+    std::getline(std::cin, input);
+    while (input.empty()) {
+        if (std::cin.eof()) {
+            std::cout << "\nExiting PhoneBook..." << std::endl;
+            exit(0);
+        }
+        std::cout << "❌ Field cannot be empty. Enter phone number: ";
+        std::getline(std::cin, input);
+    }
+    phoneNumber = input;
 
     std::cout << "Darkest secret: ";
-    std::getline(std::cin, darkestSecret);
+    std::getline(std::cin, input);
+    while (input.empty()) {
+        if (std::cin.eof()) {
+            std::cout << "\nExiting PhoneBook..." << std::endl;
+            exit(0);
+        }
+        std::cout << "❌ Field cannot be empty. Enter darkest secret: ";
+        std::getline(std::cin, input);
+    }
+    darkestSecret = input;
 }
 
 void Contact::displayShort(int index) const {
